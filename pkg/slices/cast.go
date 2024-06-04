c7448d7ad3483f059e107076e6b67058eea730a9
Gvidow <arzamachev.art-alex@yandex.ru>
2024-06-04 11:00:36 +0300

    dev fix: to pointers

--- pkg/slices/cast.go
+++ pkg/slices/cast.go
@@ -2,8 +2,8 @@ package slices
 
 func ToPointers[S ~[]E, P []*E, E any](s S) P {
 	res := make([]*E, len(s))
-	for ind, elem := range s {
-		res[ind] = &elem
+	for ind := range s {
+		res[ind] = &s[ind]
 	}
 	return res
 }
